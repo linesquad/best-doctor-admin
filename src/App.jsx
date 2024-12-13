3547d8e676959d22262d5f58b281608a7d90ef8d
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-12-13 16:51:59 +0400

    implement eslint plugins for imports

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import {
   Route,
   Outlet,
 } from "react-router-dom";
+
 import Adminlayout from "./components/Adminlayout";
-import Login from "./pages/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Home from "./pages/Home";
 import AboutMe from "./pages/AboutMe";
+import Awards from "./pages/Awards";
 import Education from "./pages/Education";
 import Experience from "./pages/Experience";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Skills from "./pages/Skills";
-import Awards from "./pages/Awards";
 
 const routes = [
   { path: "/login", element: <Login /> },
@@ -40,7 +41,7 @@ function App() {
   return (
     <Router>
       <Routes>
-      {routes.map((route, index) => (
+        {routes.map((route, index) => (
           <Route key={index} path={route.path} element={route.element}>
             {route.children?.map((child, idx) => (
               <Route key={idx} path={child.path} element={child.element} />
