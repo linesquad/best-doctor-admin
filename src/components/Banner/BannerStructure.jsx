5d34bc21d5e8df3f64ea1392d5287d9d3f47e811
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2025-01-02 22:07:41 +0400

    implement Banner's fixed design

---
+++ src/components/Banner/BannerStructure.jsx
@@ -0,0 +1,12 @@
+
+
+function BannerStructure() {
+  return (
+    <div className="bg-softBlue text-[4rem] font-semibold py-[3.81rem] pl-[3.81rem] break-words mt-[8rem]">
+      24 Years of Experience with 120+
+      Surgeries Successfully Done !
+    </div>
+  )
+}
+
+export default BannerStructure
