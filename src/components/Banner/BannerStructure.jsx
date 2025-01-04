59299c63de50410a1c5769a4bf7ee744ba50740e
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2025-01-04 13:38:27 +0400

    implement full bannerStructure functionality and validation

--- src/components/Banner/BannerStructure.jsx
+++ src/components/Banner/BannerStructure.jsx
@@ -5,20 +5,28 @@ import useUpdateBanner from "../../hooks/useBanner/useUpdateBanner";
 
 function BannerStructure() {
   const { data, error, isError, isLoading } = useGetBanner();
-  const { updateDescription} = useUpdateBanner();
+  const { updateDescription } = useUpdateBanner();
 
-  const [updateText, setUpdateText] = useState(false)
-  const bannerId = data?.description[0]?.id; 
-  const currentDescription = data?.description[0]?.description;  
-  console.log(bannerId,currentDescription);
-  
+  const [updateText, setUpdateText] = useState(false);
+  const [buttonChange, setButtonChange] = useState(false);
+  const [editError, setEditError] = useState("");
+  const { id, description } = data?.description[0] || {};
 
   const handleUpdateDescription = (e) => {
-    e.preventDefault()
-    if (!bannerId) return; 
-    setUpdateText(prev => !prev)  
+    e.preventDefault();
+    if (!id) return;
 
-    updateDescription({id: bannerId, description:currentDescription});
+    const formData = new FormData(e.target);
+    const text = formData.get("text");
+
+    if (!text || text.trim().length < 5) {
+      setEditError("Text must be at least 5 characters long.");
+    } else {
+      setUpdateText((prev) => !prev);
+      setButtonChange((prev) => !prev);
+      setEditError("");
+      updateDescription({ id: id, description: text });
+    }
   };
 
   if (isLoading) {
@@ -29,16 +37,44 @@ function BannerStructure() {
     return <p>{error.message}</p>;
   }
 
+  function handleEdit() {
+    setUpdateText((prev) => !prev);
+    setButtonChange((prev) => !prev);
+  }
+
   return (
     <>
-      <div className="bg-softBlue text-[4rem] font-semibold py-[3.81rem] pl-[3.81rem] break-words mt-[8rem]">
-        {currentDescription}
-      </div>
-      <form >
-      <button onClick={handleUpdateDescription}>
-      dak
-      </button>
-      {updateText ? <p>{currentDescription}</p> : <input type="text" name='text'/>}
+      <form onSubmit={handleUpdateDescription}>
+        <div className="bg-softBlue py-[3.81rem] px-[3.81rem] break-words mt-[8rem] flex justify-between items-center">
+          <div className="w-3/4 font-semibold text-[4rem]">
+            {updateText ? (
+              <input
+                type="text"
+                name="text"
+                placeholder="Update description"
+                className={`placeholder:font-normal pl-6 text-[2rem] rounded-lg h-[4rem] ${editError && " border-red-500 border-2"}`}
+                defaultValue={description}
+              />
+            ) : (
+              description
+            )}
+            {editError && (
+              <p className="text-red-500 text-[1rem] font-normal">
+                {editError}
+              </p>
+            )}
+          </div>
+
+          {buttonChange ? (
+            <button type="submit" className="text-[1.5rem]">
+              Save
+            </button>
+          ) : (
+            <p onClick={handleEdit} className="text-[1.5rem]">
+              Edit Text
+            </p>
+          )}
+        </div>
       </form>
     </>
   );
