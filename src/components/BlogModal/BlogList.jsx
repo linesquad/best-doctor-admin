43fbb8b0388c8a23dca1bb82f666577710436a25
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-01-06 17:08:53 +0400

    implement blog card

--- src/components/BlogModal/BlogList.jsx
+++ src/components/BlogModal/BlogList.jsx
@@ -1,3 +1,5 @@
+import BlogCard from "./BlogCard";
+
 function BlogList({ dataList, handleDelete }) {
   console.log(dataList);
 
@@ -10,38 +12,7 @@ function BlogList({ dataList, handleDelete }) {
       <div className="w-full mb-[10rem]">
         <div className="grid grid-cols-1 gap-[8rem] justify-items-center w-full mt-8">
           {dataList.map((item) => (
-            <div
-              key={item.id}
-              className="w-full rounded-[15px]   shadow-custom-light"
-            >
-              <div
-                className="w-full h-[350px] object-cover object-center bg-no-repeat rounded-t-[15px]"
-                style={{
-                  backgroundImage: `url(${item.picture})`,
-                }}
-              ></div>
-
-              <div className="text-left bg-white w-full p-4 rounded-b-[15px] border-b border-blog-border">
-                <p className="text-[#808080] text-[20px] font-heeboBold leading-[135%] mb-2">
-                  {item.time}
-                </p>
-                <p className="text-[32px] font-poppinsBold leading-tight mb-2">
-                  {item.title}
-                </p>
-                <p className="font-heeboRegular text-[18px] leading-relaxed mb-4 break-words whitespace-normal">
-                  {item.slug}
-                </p>
-                <p className="font-heeboRegular text-[18px] leading-relaxed mb-4 break-words whitespace-normal">
-                  {item.content}
-                </p>
-                <p
-                  className="text-[32px] cursor-pointer text-red-500"
-                  onClick={() => handleDelete(item.id)}
-                >
-                  x
-                </p>
-              </div>
-            </div>
+            <BlogCard data={item} key={item.id} handleDelete={handleDelete} />
           ))}
         </div>
       </div>
