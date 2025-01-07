497e0501ccff874e3f5562a4efff8200b38829bd
BYCODDE <anaraliev217@gmail.com>
2025-01-07 21:12:23 +0400

    tornike's component added!

--- src/components/BlogModal/BlogCard.jsx
+++ src/components/BlogModal/BlogCard.jsx
@@ -1,34 +1,38 @@
+
 function BlogCard({ data, handleDelete }) {
   return (
-    <div className="w-full rounded-[15px]   shadow-custom-light">
-      <div
-        className="w-full h-[350px] object-cover object-center bg-no-repeat rounded-t-[15px]"
-        style={{
-          backgroundImage: `url(${data.picture})`,
-        }}
-      ></div>
+    <>
+      <div className="w-full rounded-[15px]   shadow-custom-light">
+        <div
+          className="w-full h-[350px] object-cover object-center bg-no-repeat rounded-t-[15px]"
+          style={{
+            backgroundImage: `url(${data.picture})`,
+          }}
+        ></div>
 
-      <div className="text-left bg-white w-full p-4 rounded-b-[15px] border-b border-blog-border">
-        <p className="text-[#808080] text-[20px] font-heeboBold leading-[135%] mb-2">
-          Reading Time: {data.time} minutes
-        </p>
-        <p className="text-[32px] font-poppinsBold leading-tight mb-2">
-          {data.title}
-        </p>
-        <p className="font-heeboRegular text-[18px] leading-relaxed mb-4 break-words whitespace-normal">
-          {data.slug}
-        </p>
-        <p className="font-heeboRegular text-[18px] leading-relaxed mb-4 break-words whitespace-normal">
-          {data.content}
-        </p>
-        <p
-          className="text-[32px] cursor-pointer text-red-500"
-          onClick={() => handleDelete(data.id)}
-        >
-          x
-        </p>
+        <div className="text-left bg-white w-full p-4 rounded-b-[15px] border-b border-blog-border">
+          <p className="text-[#808080] text-[20px] font-heeboBold leading-[135%] mb-2">
+            Reading Time: {data.time} minutes
+          </p>
+          <p className="text-[32px] font-poppinsBold leading-tight mb-2">
+            {data.title}
+          </p>
+          <p className="font-heeboRegular text-[18px] leading-relaxed mb-4 break-words whitespace-normal">
+            {data.slug}
+          </p>
+          <p className="font-heeboRegular text-[18px] leading-relaxed mb-4 break-words whitespace-normal">
+            {data.content}
+          </p>
+          <p
+            className="text-[32px] cursor-pointer text-red-500"
+            onClick={() => handleDelete(data.id)}
+          >
+            x
+          </p>
+        </div>
       </div>
-    </div>
+
+    </>
   );
 }
 
--- src/components/BlogModal/BlogList.jsx
+++ src/components/BlogModal/BlogList.jsx
@@ -1,20 +1,30 @@
 import BlogCard from "./BlogCard";
+import HomeCarousel from "../home/Carousel/HomeCarousel";
 
 function BlogList({ dataList, handleDelete }) {
   return (
-    <div className="md:p-[40px]">
-      <h1 className="md:text-left   md:text-[64px] text-[44px] text-center     font-bold text-[#000] font-poppins  mb-[10rem] mt-[10rem]">
-        Suggested For You
-      </h1>
+    <>
+      <div className="md:p-[40px]">
+        <h1 className="md:text-left   md:text-[64px] text-[44px] text-center     font-bold text-[#000] font-poppins  mb-[10rem] mt-[10rem]">
+          Suggested For You
+        </h1>
 
-      <div className="w-full mb-[10rem]">
-        <div className="grid grid-cols-1 gap-[8rem] justify-items-center w-full mt-8">
-          {dataList.map((item) => (
-            <BlogCard data={item} key={item.id} handleDelete={handleDelete} />
-          ))}
+        <div className="w-full mb-[10rem]">
+          <div className="grid grid-cols-1 gap-[8rem] justify-items-center w-full mt-8">
+            {dataList.map((item) => (
+              <BlogCard data={item} key={item.id} handleDelete={handleDelete} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+      <div>
+        <h3 className="md:text-left   md:text-[64px] text-[44px] text-center     font-bold text-[#000] font-poppins  mb-[5rem] mt-[10rem]">
+          Suggested For You
+        </h3>
+
+        <HomeCarousel></HomeCarousel>
+      </div>
+    </>
   );
 }
 
