ab0784a87b0f820eae580f92a527e31945e3d7de
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2025-01-08 23:53:06 +0400

    implement design and paragraph stucture of blog page

--- src/pages/SingleBlogContent.jsx
+++ src/pages/SingleBlogContent.jsx
@@ -4,22 +4,49 @@ import { useGetBlog } from "../hooks/useBlog/useGetBlog";
 function SingleBlogContent() {
   const { id } = useParams();
   const { data, isError, isLoading, error } = useGetBlog(id);
-  
-    if (isLoading) return <p>Loading...</p>;
-    if (isError) return <p>Error: {error.message}</p>;
-    
-  const dataBlog = data.blog
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error: {error.message}</p>;
+
+  const dataBlog = data.blog;
+
+  const contentParts = dataBlog.content.split("/");
 
   return (
-    <div className="single-blog-content">
-      <h1 className="text-4xl font-bold mb-4">{dataBlog.title}</h1>
-      <p className="text-lg text-gray-700 mb-6">{dataBlog.content}</p>
+    <div className="single-blog-content px-4 md:px-16 lg:px-32 mt-10">
+      <h1 className="text-4xl font-bold mb-4 text-center">{dataBlog.title}</h1>
+
+      <p className="text-xs text-gray-500 mb-4">
+        Reading Time: {dataBlog.time} minutes
+      </p>
+
+      {contentParts
+        .slice(0, Math.floor(contentParts.length / 2))
+        .map((part, index) => (
+          <p
+            key={`content-top-${index}`}
+            className="text-lg text-gray-700 mb-6"
+          >
+            {part.trim()}
+          </p>
+        ))}
+
       <img
         src={dataBlog.picture}
         alt={dataBlog.title}
         className="w-full h-auto rounded-md mb-6"
       />
-      <p className="text-sm text-gray-500">Reading Time: {dataBlog.time} minutes</p>
+
+      {contentParts
+        .slice(Math.floor(contentParts.length / 2))
+        .map((part, index) => (
+          <p
+            key={`content-bottom-${index}`}
+            className="text-lg text-gray-700 mb-6"
+          >
+            {part.trim()}
+          </p>
+        ))}
     </div>
   );
 }
