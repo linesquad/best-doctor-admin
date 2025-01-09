7d1bdbbc69c2da0584faec7ad20e534f4d64a673
tornike2002 <toko.toka2000@gmail.com>
2025-01-09 18:39:02 +0400

    add Error display and skeleton

--- src/components/blog/hero/BlogHero.jsx
+++ src/components/blog/hero/BlogHero.jsx
@@ -1,9 +1,11 @@
 import BlogHeroContent from "./BlogHeroContent";
+import BlogHeroSkeleton from "./BlogHeroSkeleton";
 import useGetBlogHero from "../../../hooks/useGetBlogHero";
+import ErrorDisplay from "../../ErrorDisplay";
 function BlogHero() {
   const { data, isLoading, isError, error } = useGetBlogHero();
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>Error: {error.message}</div>;
+  if (isLoading) return <BlogHeroSkeleton />;
+  if (isError) return <ErrorDisplay errorMsg={error.message} />;
 
   return (
     <div>
