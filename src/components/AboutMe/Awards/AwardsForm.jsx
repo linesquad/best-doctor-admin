b7c7d826f54533d0e9243f284911e7a496942516
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2025-01-16 13:35:57 +0400

    implement AwardAddModal full functionality and UI

--- src/components/AboutMe/Awards/AwardsForm.jsx
+++ src/components/AboutMe/Awards/AwardsForm.jsx
@@ -1,68 +1,23 @@
+
+import AwardsAddModal from "./AwardsAddModal";
 import CustomButton from "../../../ui/CustomButton";
 
-function AwardsForm({ handleAddAwards }) {
+function AwardsForm({ handleAddAwards,showAddModal,setShowAddModal,handleAddCancel }) {
   return (
     <div>
-      <form onSubmit={handleAddAwards} action="">
-        <div>
-          <label className="block text-sm font-semibold text-gray-700 mb-2">
-            Award Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            placeholder="Enter Award name"
-            className="w-full px-4 py-3 border-2 border-pastelBlue rounded-lg focus:ring-2 focus:ring-brightBlue focus:border-brightBlue transition duration-200 outline-none"
-          />
-        </div>
-
-        <div>
-          <label className="block text-sm font-semibold text-gray-700 mb-2">
-            Date
-          </label>
-          <input
-            type="date"
-            name="date"
-            placeholder="Date"
-            rows="4"
-            className="w-full px-4 py-3 border-2 border-pastelBlue rounded-lg focus:ring-2 focus:ring-brightBlue focus:border-brightBlue transition duration-200 outline-none resize-none"
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-semibold text-gray-700 mb-2">
-            Awarded By
-          </label>
-          <textarea
-            name="awarded"
-            placeholder="Awarded By"
-            rows="4"
-            className="w-full px-4 py-3 border-2 border-pastelBlue rounded-lg focus:ring-2 focus:ring-brightBlue focus:border-brightBlue transition duration-200 outline-none resize-none"
-          />
-        </div>
-
-        <div className="flex justify-end gap-4 pt-4">
-          <CustomButton
-            name="Cancel"
-            color="text-darkBlue"
-            bg="bg-crystalBlue hover:bg-pastelBlue"
-            paddingX="px-6"
-            paddingY="py-2.5"
-            rounded="rounded-lg"
-            type="button"
-            // onClick={handleAddCancel}
-          />
-
-          <CustomButton
-            name="Add Award"
-            color="text-white"
-            bg="bg-brightBlue hover:bg-oceanBlue"
-            paddingX="px-6"
-            paddingY="py-2.5"
-            rounded="rounded-lg"
-            type="submit"
-          />
-        </div>
-      </form>
+      <CustomButton
+        name="Add New Award"
+        color="text-lightGrey"
+        weight={"font-normal"}
+        bg="bg-darkBlue"
+        paddingX="px-10"
+        paddingY="py-3"
+        rounded="rounded-[3rem]"
+        maxW={"w-full lg:w-[30rem]"}
+        onClick={() => setShowAddModal(true)}
+        scale={"hover:scale-105"}
+      />
+      <AwardsAddModal handleAddAwards={handleAddAwards} showAddModal={showAddModal} handleAddCancel={handleAddCancel}/>
     </div>
   );
 }
--- src/components/AboutMe/Awards/AwardsStructure.jsx
+++ src/components/AboutMe/Awards/AwardsStructure.jsx
@@ -1,20 +1,28 @@
+import { useState } from "react";
+
 import AwardsForm from "./AwardsForm";
 import AwardsList from "./AwardsList";
 import { useAddAwards } from "../../../hooks/useAwards/useAddAwards";
 import { useGetAwards } from "../../../hooks/useAwards/useGetAwards";
 
 function AwardsStructure() {
+  const [showAddModal, setShowAddModal] = useState(false);
   const { data, isError, isLoading, error } = useGetAwards();
-  const {mutate: addAwards} = useAddAwards()
+  const { mutate: addAwards } = useAddAwards();
+
+  const handleAddCancel = () => {
+    setShowAddModal(false);
+  };
 
   const handleAddAwards = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const formData = new FormData(e.target);
     const newAward = formData.get("name");
     const newDate = formData.get("date");
     const newAwarded = formData.get("awarded");
     addAwards({ name: newAward, date: newDate, awardedBy: newAwarded });
-  }
+    handleAddCancel();
+  };
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error: {error.message}</div>;
@@ -22,8 +30,13 @@ function AwardsStructure() {
 
   return (
     <div>
-      <AwardsList data={data.award}/>
-      <AwardsForm handleAddAwards={handleAddAwards}/>
+      <AwardsList data={data.award} />
+      <AwardsForm
+        handleAddCancel={handleAddCancel}
+        handleAddAwards={handleAddAwards}
+        showAddModal={showAddModal}
+        setShowAddModal={setShowAddModal}
+      />
     </div>
   );
 }
