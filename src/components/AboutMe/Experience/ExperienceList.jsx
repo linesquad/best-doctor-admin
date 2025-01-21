d5473430c8c651da57f573b7e9f3924e4856f6fa
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2025-01-21 17:14:39 +0400

    vector icon added!

--- src/components/AboutMe/Experience/ExperienceList.jsx
+++ src/components/AboutMe/Experience/ExperienceList.jsx
@@ -2,6 +2,8 @@ import { MdModeEdit } from "react-icons/md";
 
 import ReusableTitle from "../../ReusableTitle";
 
+import vectorIcon from "/images/Vector.svg";
+
 function ExperienceList({ data, handleUpdateModal, handleDelete, circleIcon }) {
   return (
     <>
@@ -23,13 +25,22 @@ function ExperienceList({ data, handleUpdateModal, handleDelete, circleIcon }) {
             >
               {/* Container for image and content */}
               <div className="flex items-start w-full gap-4">
-                {/* Image on the left */}
-                <div className="bg-softBlue w-5 h-5 flex justify-center items-center rounded-lg">
-                  <img src={circleIcon} alt="" />
+                <div className="flex flex-col items-center">
+                  {/* Circle Icon */}
+                  <div className="bg-softBlue w-5 h-5 flex justify-center items-center rounded-lg">
+                    <img src={circleIcon} alt="" />
+                  </div>
+
+                  {/* Vector Icon - Positioned below circleIcon */}
+                  <img
+                    src={vectorIcon}
+                    className="max-h-[50px] mt-2"
+                    alt="vector"
+                  />
                 </div>
 
                 {/* Content on the right */}
-                <div className="flex flex-col">
+                <div className="flex flex-col justify-center">
                   <h2 className="font-poppinsExtraBold leading-[135%] uppercase">
                     {item.place}
                   </h2>
@@ -37,15 +48,18 @@ function ExperienceList({ data, handleUpdateModal, handleDelete, circleIcon }) {
                     <h3 className="font-poppinsExtraBold uppercase">
                       {item.department}
                     </h3>
-                    <span className="font-heeboRegular opacity-50">{`${item.from.slice(0, 4)} - ${
-                      item.to ? item.to.slice(0, 4) : "Present"
-                    }`}</span>
+                    <span className="font-heeboRegular opacity-50">{`${item.from.slice(
+                      0,
+                      4
+                    )} - ${item.to ? item.to.slice(0, 4) : "Present"}`}</span>
                   </div>
                   <h4 className="font-heeboRegular opacity-50">
                     {item.position}
                   </h4>
                 </div>
               </div>
+
+              {/* Edit/Delete Icons */}
               <div className="flex gap-3 items-center">
                 <MdModeEdit
                   size={30}
